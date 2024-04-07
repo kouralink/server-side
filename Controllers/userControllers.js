6c926e9121195ba2c5dccc38fd2cf43fef55c615
ilorez <zobirnajdaoui@gmail.com>
2024-04-07 14:52:27 +0000

    update user controllers and routes to use uid instide of using username in CRUD

--- Controllers/userControllers.js
+++ Controllers/userControllers.js
@@ -17,6 +17,8 @@ import {
 
 const db = getFirestore(firebase);
 
+
+//TODO: using UID that we got from firebase authentication to create user and get user and update user and delete user
 // check if username is unique
 const isItUniqueUsername = async (username) => {
     const q = query(collection(db, 'users'), where('username', '==', username));
@@ -24,10 +26,46 @@ const isItUniqueUsername = async (username) => {
     return querySnapshot.empty;
   };
 
-
 export const createUser = async (req, res) => {
     try {
+      // test if user uid exists
       const data = req.body;
+      // make sure that all fields are filled and make egal empty string if not expet UID and username
+      if (!data.firstName) {
+        data.firstName = '';
+      }
+      if (!data.lastName) {
+        data.lastName = '';
+      }
+      if (!data.gender) {
+        data.gender = 'male';
+      }
+      if (!data.birthday) {
+        data.birthday = '';
+      }
+      if (!data.address) {
+        data.address = '';
+      }
+      if (!data.bio) {
+        data.bio = '';
+      }
+      if (!data.phoneNumbers) {
+        data.phoneNumbers = [];
+      }
+
+
+      const UID = data.UID;
+      if (!UID) {
+        res.status(400).send('UID is required');
+        return;
+    }
+      // test if aleady there is doc with uid
+      const docRef = doc(collection(db, 'users'), req.body.UID);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        res.status(400).send('user already exists');
+        return;
+      }
         //   generate username if exists in db
         if (!data.username) {
             data.username = data.firstName + data.lastName;
@@ -36,40 +74,17 @@ export const createUser = async (req, res) => {
         while (!await isItUniqueUsername(data.username)) {
             data.username = data.username + Math.floor(Math.random() * 100);
         }
-        // using setDoc instead of addDoc 
-        await addDoc(collection(db, 'users'), data);
+        delete data.UID;
+        await setDoc(docRef, data);
         res.status(201).send('user created successfully');
     } catch (error) {
+      console.log(error);
       res.status(400).send(error.message);
     }
   };
 
-export const getUsers = async (req, res) => {
-    try {
-      const users = [];
-      const querySnapshot = await getDocs(collection(db, 'users'));
-      querySnapshot.forEach((doc) => {
-        const user = new User(
-            doc.data().username,
-            doc.data().firstName,
-            doc.data().lastName,
-            doc.data().gender,
-            doc.data().birthday,
-            doc.data().address,
-            doc.data().bio,
-            doc.data().phoneNumbers
-        );
-        users.push({"doc_id": doc.id ,"user":user});
-        }
-    );
-    res.status(200).send(users);
-    }
-    catch (error) {
-        res.status(400).send(error.message);
-    }
-}
-
-// is username unique using get method to get user by username
+  
+  // is username unique using get method to get user by username
 export const isUsernameUnique = async (req, res) => {
     try {
       const q = query(collection(db, 'users'), where('username', '==', req.params.username));
@@ -85,7 +100,7 @@ export const isUsernameUnique = async (req, res) => {
   }
 
 // using get method to get user by username
-export const getUser = async (req, res) => {
+export const getUserByUsername = async (req, res) => {
     try {
         const q = query(collection(db, 'users'), where('username', '==', req.params.username));
         const querySnapshot = await getDocs(q);
@@ -112,67 +127,71 @@ export const getUser = async (req, res) => {
     }
 }
 
+// get user by UID
+export const getUserByUID = async (req, res) => {
+    try {
+        const docRef = doc(collection(db, 'users'), req.params.uid);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+            const user = new User(
+                docSnap.data().username,
+                docSnap.data().firstName,
+                docSnap.data().lastName,
+                docSnap.data().gender,
+                docSnap.data().birthday,
+                docSnap.data().address,
+                docSnap.data().bio,
+                docSnap.data().phoneNumbers
+            );
+            res.status(200).send(user);
+        } else {
+            res.status(404).send('user not found');
+        }
+    }
+    catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
+
+// update user by UID
 export const updateUser = async (req, res) => {
     try {
-        // if (req.body.username) {
-        //     if (req.body.username !== req.params.username) {
-        //     if (await isItUniqueUsername(req.body.username)) {
-        //         const q = query(collection(db, 'users'), where('username', '==', req.params.username));
-        //         const querySnapshot = await getDocs(q); 
-        //         if (!querySnapshot.empty) {
-        //             const doc = querySnapshot.docs[0];
-        //             // update using set docs method
-        //             await setDoc(doc.ref, req.body, { merge: true });
-        //         } else {
-        //             res.status(404).send('user not found');
-        //         }
-        //         res.status(200).send('user updated successfully');
-        //     } else {
-        //         res.status(400).send('username already exists');
-        //     }
-        // }
-
-        // }
-
-        if (req.body.username) {
-            if (req.body.username !== req.params.username) {
-                if (!await isItUniqueUsername(req.body.username)) {
-                    res.status(400).send('username already exists');
-                    return;
+        const docRef = doc(collection(db, 'users'), req.params.uid);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+            // test of username is unique
+            if (req.body.username) {
+                if (req.body.username !== docSnap.data().username) {
+                    if (!await isItUniqueUsername(req.body.username)) {
+                        res.status(400).send('username already exists');
+                        return;
+                    }
                 }
             }
-            
-        }
-        const q = query(collection(db, 'users'), where('username', '==', req.params.username));
-        const querySnapshot = await getDocs(q);
-        if (!querySnapshot.empty) {
-            const doc = querySnapshot.docs[0];
-            // update using set docs method
-            await setDoc(doc.ref, req.body, { merge: true });
+            await setDoc(docRef, req.body, { merge: true });
+            res.status(200).send('user updated successfully');
         } else {
             res.status(404).send('user not found');
         }
-      res.status(200).send('user updated successfully');
     } catch (error) {
       res.status(400).send(error.message);
     }
-  };
+  }
 
+// delete by UID
 export const deleteUser = async (req, res) => {
     try {
-        // find doc by username and delete document with a random id
-        const q = query(collection(db, 'users'), where('username', '==', req.params.username));
-        const querySnapshot = await getDocs(q);
-        if (!querySnapshot.empty) {
-            const doc = querySnapshot.docs[0];
-            await deleteDoc(doc.ref);
-            res.status(200).send('user deleted successfully');
-        } else {
-            res.status(404).send('user not found');
-        }
+      const docRef = doc(collection(db, 'users'), req.params.uid);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        await deleteDoc(docRef);
+        res.status(200).send('user deleted successfully');
+      } else {
+        res.status(404).send('user not found');
+      }
     } catch (error) {
       res.status(400).send(error.message);
     }
   };
-
 
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,15 +1,16 @@
 import express from 'express';
-import { getUsers, getUser, isUsernameUnique, createUser, updateUser, deleteUser } from '../Controllers/userControllers.js'
+import { getUserByUsername,getUserByUID, isUsernameUnique, createUser, updateUser, deleteUser } from '../Controllers/userControllers.js'
 
 
 const router = express.Router();
 
-router.get('/', getUsers);
-router.get('/:username', getUser);
+// router.get('/', getUsers);
+router.get('/get/by/username/:username', getUserByUsername);
+router.get('/get/by/uid/:uid', getUserByUID);
 router.get('/isUsernameUnique/:username', isUsernameUnique);
 router.post('/', createUser);
-router.put('/:username', updateUser);
-router.delete('/:username', deleteUser);
+router.put('/:uid', updateUser);
+router.delete('/:uid', deleteUser);
 
 
 export default router;
